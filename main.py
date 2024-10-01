12ee22f7f23c68c85e32b6b330f25d44e41344df
spawned18 <spawnedlive@gmail.com>
2024-10-01 16:29:16 -0500

    added character report function for printing character counts

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        # print(file_contents)
         word_count = count_words(file_contents)
         character_count = count_characters(file_contents)
-        print(word_count, character_count)
+        character_report(character_count, word_count)
 
 def count_words(text):
     words = text.split()
@@ -20,4 +20,18 @@ def count_characters(text):
         else:
             character_count[character] += 1
     return character_count
+
+def character_report(character_count, word_count):
+    sorted_characters = [
+        {"char": char, "num": count}
+        for char, count in character_count.items() if char.isalpha()
+    ]
+    sorted_characters.sort(key=lambda x: x['num'], reverse=True)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document\n")
+    for char_info in sorted_characters:
+        print(f"The '{char_info['char']}' character was found {char_info['num']} times")
+    print("\n--- End report ---")
+
 main()
