e0642617fa7155e9cce7e90b20faabda7316b54a
spawned18 <spawnedlive@gmail.com>
2024-09-30 20:04:35 -0500

    added character count function

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ def main():
         file_contents = f.read()
         print(file_contents)
         word_count = count_words(file_contents)
-        print(word_count)
+        character_count = count_characters(file_contents)
+        print(word_count, character_count)
 
 def count_words(text):
     words = text.split()
     word_count = len(words)
     return word_count
 
+def count_characters(text):
+    character_count = {}
+    for character in text:
+        character = character.lower()
+        if character not in character_count:
+            character_count[character] = 1
+        else:
+            character_count[character] += 1
+    return character_count
 main()
